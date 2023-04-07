8641650c829038f0733e3c1074ebc2508cd71c53
Oscar Rivera <orivra@getontop.com>
2023-04-06 22:42:13 -0500

    mongo connection error fixed, and changes reverted

--- applications/app-service/src/main/java/co/com/evermore/MainApplication.java
+++ applications/app-service/src/main/java/co/com/evermore/MainApplication.java
@@ -5,15 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-        MongoAutoConfiguration.class,
-        MongoDataAutoConfiguration.class
-})
+@SpringBootApplication
 public class MainApplication {
 
     public static void main(String[] args) {
-        System.setProperty("javax.net.ssl.trustStore", "none");
-        System.setProperty("jdk.tls.disabledAlgorithms", "");
         SpringApplication.run(MainApplication.class, args);
     }
 }
