b550e0698e2c20b27806aeb9f7f9b6cf04a5c0dd
Oscar Rivera <orivra@getontop.com>
2023-04-06 21:57:35 -0500

    updated with mongo auth fix

--- applications/app-service/src/main/java/co/com/evermore/MainApplication.java
+++ applications/app-service/src/main/java/co/com/evermore/MainApplication.java
@@ -10,7 +10,9 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
         MongoDataAutoConfiguration.class
 })
 public class MainApplication {
+
     public static void main(String[] args) {
+        System.setProperty("javax.net.ssl.trustStore", "none");
         SpringApplication.run(MainApplication.class, args);
     }
 }
